5197e24cfc789b09244979b02370dd07db2c735a
Maxwel <maxwelsant96@icloud.com>
2021-10-16 11:34:34 -0300

    password hashing bycript

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,12 +5,16 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
-
+import * as bycript from 'bcrypt';
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    const salt = await bycript.genSalt();
+    const hashedPassword = await bycript.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {
